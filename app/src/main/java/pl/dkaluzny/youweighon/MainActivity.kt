4e24335736a528c23f56dda950f524b490719f26
kalug89 <kaluzny_david@hotmail.com>
2018-07-21 20:44:15 +0200

    Add checkbox, and add logcat message from checkbox

--- app/src/main/java/pl/dkaluzny/youweighon/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/youweighon/MainActivity.kt
@@ -2,11 +2,18 @@ package pl.dkaluzny.youweighon
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     val marsGravity = 0.38
+    val venusGravity = 0.91
+    val jupiterGravity = 2.34
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,21 @@ class MainActivity : AppCompatActivity() {
             var result = calculateWeight(weight.toString().toDouble())
 
             resultTextViewvId.text = "Yout weight " + result.toString() + " on Mars"
+        }
+        marsCheckBox.setOnClickListener(this)
+        venusCheckBox.setOnClickListener(this)
+        jupiterCheckBox.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        v as CheckBox
+        var isChecked:Boolean = v.isChecked
+
+        when(v.id) {
+            R.id.marsCheckBox -> if (isChecked){Log.wtf("Setup Mars", "Mars")}
+            R.id.venusCheckBox -> if (isChecked){Log.wtf("Stup Venus", "Venus")}
+            R.id.jupiterCheckBox -> if (isChecked){Log.wtf("Stup Jupiter", "Jupiter")}
         }
     }
 
