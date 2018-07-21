628507a163a473a08f0e750581708188e4f7a66e
kalug89 <kaluzny_david@hotmail.com>
2018-07-21 21:24:38 +0200

    Add planet and logic to program

--- app/src/main/java/pl/dkaluzny/youweighon/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/youweighon/MainActivity.kt
@@ -9,40 +9,45 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-
     val marsGravity = 0.38
     val venusGravity = 0.91
     val jupiterGravity = 2.34
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var weight = enterWeightId.text
         showWeightButtonId.setOnClickListener {
-            var result = calculateWeight(weight.toString().toDouble())
-
-            resultTextViewvId.text = "Yout weight " + result.toString() + " on Mars"
+//        var result = calculateWeight(weight.toString().toDouble())
+//         resultTextViewvId.text = "Yout weight " + result.toString() + " on Mars"
         }
         marsCheckBox.setOnClickListener(this)
         venusCheckBox.setOnClickListener(this)
         jupiterCheckBox.setOnClickListener(this)
-
     }
 
     override fun onClick(v: View?) {
         v as CheckBox
         var isChecked:Boolean = v.isChecked
+        var weight = enterWeightId.text
 
         when(v.id) {
-            R.id.marsCheckBox -> if (isChecked){Log.wtf("Setup Mars", "Mars")}
-            R.id.venusCheckBox -> if (isChecked){Log.wtf("Stup Venus", "Venus")}
-            R.id.jupiterCheckBox -> if (isChecked){Log.wtf("Stup Jupiter", "Jupiter")}
+            R.id.marsCheckBox -> if (isChecked){calculateWeight(weight.toString().toDouble(), v)}
+            R.id.venusCheckBox -> if (isChecked){calculateWeight(weight.toString().toDouble(), v)}
+            R.id.jupiterCheckBox -> if (isChecked){calculateWeight(weight.toString().toDouble(), v)}
         }
     }
 
-    fun calculateWeight(userWeight: Double): Double {
-        return userWeight * marsGravity
+    fun calculateWeight(userWeight: Double, checkBox: CheckBox) {
+        var result: Double?
+        when(checkBox.id){
+
+            R.id.marsCheckBox -> result =  userWeight * marsGravity
+            R.id.venusCheckBox -> result =  userWeight * venusGravity
+            R.id.jupiterCheckBox -> result =  userWeight * jupiterGravity
+            else -> result = userWeight * marsGravity
+        }
+
+        resultTextViewvId.text =  "Weight is" + result
     }
 }
